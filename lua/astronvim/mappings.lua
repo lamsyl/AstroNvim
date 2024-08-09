135867b73d78cf53583e7702048af9354d81dbda
yuklam <lamsungdev@gmail.com>
2024-08-09 15:42:02 +0800

    remove alpha home screen mapping

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -129,20 +129,6 @@ maps.n["[t"] = { function() vim.cmd.tabprevious() end, desc = "Previous tab" }
 maps.n["]D"] = { ']c', desc = "Next diff" }
 maps.n["[D"] = { '[c', desc = "Previous diff" }
 
--- Alpha
-if is_available "alpha-nvim" then
-  maps.n["<leader>h"] = {
-    function()
-      local wins = vim.api.nvim_tabpage_list_wins(0)
-      if #wins > 1 and vim.bo[vim.api.nvim_win_get_buf(wins[1])].filetype == "neo-tree" then
-        vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
-      end
-      require("alpha").start(false)
-    end,
-    desc = "Home Screen",
-  }
-end
-
 -- Comment
 if is_available "Comment.nvim" then
   maps.n["<leader>/"] = {
